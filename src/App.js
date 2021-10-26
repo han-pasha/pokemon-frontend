6fdd56718be38eed038c9f6e740bc0b10a287ed2
HAN PASHA <crownsace@gmail.com>
2021-10-26 10:14:45 +0700

    Stable 0.2, All UI/UX Issues has been resolved

--- src/App.js
+++ src/App.js
@@ -15,10 +15,28 @@ const App = () => {
   const { user } = useAuth0();
 
   //USER INFO
-  const [userInfo, setUserInfo] = useState();
+  const [userInfo, setUserInfo] = useState(" ");
   const [message, setMessage] = useState("");
-  const [pokemonName, setPokemonName] = useState();
 
+  //POKEMON INFO
+  const [pokemonName, setPokemonName] = useState();
+  const [pokemonStats, setPokemonStats] = useState([]);
+
+  //INPUT FORMS
+  const [value, setValue] = useState();
+
+  /* 
+    USING TWO EFFECT
+    THE FIRST ONE IS TO POPULATE THE STATE
+    BECAUSE IT HAS EMPETY DEPENDENCE IT
+    WOULD ONLY CALLED ONCE 
+  */
+  useEffect(() => {
+    callRandomApi();
+  }, []);
+
+  // THE SECOND ONE FOR GETTING USER INFORMATION
+  // EACH TIME isAuthenticated CALLED, IT WOULD REFRESH
   useEffect(() => {
     if (user !== undefined) {
       const { name, picture, email } = user;
@@ -26,20 +44,40 @@ const App = () => {
     }
   }, [isAuthenticated]);
 
-  const callApi = async () => {
+  // FOR CALLING API WITHOUT AUTH
+  const callApi = async (pokemon) => {
+    try {
+      const response = await fetch(`http://localhost:7000/pokemon/${pokemon}`);
+      const responseData = await response.json();
+      setMessage(responseData.message);
+      setPokemonName(responseData.name);
+
+      // IF UNDEFINED. MAP WOULD BE ERROR
+      if (responseData.stats !== undefined) {
+        setPokemonStats(responseData.stats);
+      }
+      console.log(responseData.stats);
+    } catch (error) {
+      setMessage(error.message);
+    }
+  };
+
+  const callRandomApi = async () => {
     try {
       var randNumber = Math.floor(Math.random() * (600 - 0) + 0);
       const response = await fetch(
         `http://localhost:7000/pokemon/${randNumber}`
       );
       const responseData = await response.json();
-      console.log(responseData);
       setMessage(responseData.message);
       setPokemonName(responseData.name);
+      setPokemonStats(responseData.stats);
+      console.log(responseData.stats);
     } catch (error) {
       setMessage(error.message);
     }
   };
+
 
   const callSecureApi = async () => {
     try {
@@ -54,9 +92,38 @@ const App = () => {
       const responseData = await response.json();
       console.log(responseData);
       setMessage(responseData.message);
+      setPokemonName(responseData.name);
+      setPokemonStats(responseData.stats);
     } catch (error) {
       setMessage(error.message);
     }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (value > 1100) {
+      //LIMIT
+      setMessage("VALUE KEJAUHAN");
+    }
+    callApi(value);
+  };
+  const listStat = () => {
+    return (
+      <table className="table">
+        <tbody>
+          <tr>
+            <th scope="col"> STAT</th>
+            <td>Value</td>
+          </tr>
+          {pokemonStats.map((stat, index) => (
+            <tr key={index}>
+              <th scope="col">{stat.stat.name}</th>
+              <td>{stat.base_stat}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
   };
 
   return (
@@ -65,14 +132,7 @@ const App = () => {
         <nav className="navbar navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              {/* <img
-                src="/docs/5.1/assets/brand/bootstrap-logo.svg"
-                alt=""
-                width="30"
-                height="24"
-                class="d-inline-block align-text-top"
-              /> */}
-              Bootstrap
+              Pokemon
             </a>
             <a>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</a>
           </div>
@@ -81,64 +141,50 @@ const App = () => {
 
       <div className="main-area">
         <h5>Hallo {userInfo}</h5>
-        <input
-          className="form-control"
-          type="text"
-          value="Readonly input here..."
-          aria-label="readonly input example"
-          readOnly
-        />
-        <div className="grid-pokemons">
-          <button
-            type="button"
-            className="btn btn-success w-75 item1-area"
-            onClick={callApi}
-          >
-            Get Pokemon
-          </button>
-          <button
-            type="button"
-            className="btn btn-success w-75 item2-area"
-            onClick={callSecureApi}
-          >
-            Get Random Pokemon
-          </button>
-          <button
-            type="button"
-            className="btn btn-success w-75 item3-area"
-            disabled={!isAuthenticated}
-            onClick={callSecureApi}
-          >
-            Get Spesific Pokemon
-          </button>
-          <button
-            type="button"
-            className="btn btn-success w-75 item4-area"
-            disabled={!isAuthenticated}
-          >
-            Get Unique Pokemon
-          </button>
-        </div>
+        <form onSubmit={onSubmit}>
+          <input
+            className="form-control"
+            type="text"
+            value={value}
+            placeholder="input using it's name or just a number"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <div className="grid-pokemons">
+            <button type="submit" className="btn btn-success w-75 item1-area">
+              Get Pokemon
+            </button>
+            <button
+              type="button"
+              className="btn btn-success w-75 item2-area"
+              value="submit"
+              onClick={callRandomApi}
+            >
+              Get Random Pokemon
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger w-75 item3-area"
+              disabled={!isAuthenticated}
+              onClick={callSecureApi}
+            >
+              Get Spesific Pokemon
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger w-75 item4-area"
+              disabled={!isAuthenticated}
+              onClick={callSecureApi}
+            >
+              Get Unique Pokemon
+            </button>
+          </div>
+        </form>
+        <h2 style={{ marginBottom: "20px", textAlign: "center" }}>{message}</h2>
 
-        <h3>{pokemonName}</h3>
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Name</th>
-              <th scope="col">Type 1</th>
-              <th scope="col">Type 2</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{pokemonName}</td>
-              <td>
-                <img src="https://pokeres.bastionbot.org/images/pokemon/1.png" />
-              </td>
-              <td>@mdo</td>
-            </tr>
-          </tbody>
-        </table>
+        <h2 style={{ marginBottom: "20px", textAlign: "center" }}>
+          {pokemonName}
+        </h2>
+        {listStat()}
       </div>
       <div className="footer-area">
         <Footer />
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="bg-light p-3 text-center">
+  <footer className="bg-light p-3 text-center footer">
     <div className="logo" />
     <p>
-      Sample project provided by{" "}
+      Sample project created by{" "}
       <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
-        Auth0
+        Han
       </a>
     </p>
   </footer>
